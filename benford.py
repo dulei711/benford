b009aa8aa818ba9f44593388654af26c2c9965e5
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-08 16:24:16 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -1,49 +1,56 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
-from streamlit_pandas_profiling import st_profile_report
-from newcomb_benford import detect
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def load_data():
-    file = st.file_uploader("Upload file", type=["xlsx", "xls"])
-    if file is not None:
-        df = pd.read_excel(file)
-        return df
+# Function to calculate the frequency of the first, second, and third digits
+def calc_freq(data):
+    # Get the first digit of each number
+    first_digit = data.apply(lambda x: int(str(x)[0]))
+    # Get the second digit of each number
+    second_digit = data.apply(lambda x: int(str(x)[1]))
+    # Get the third digit of each number
+    third_digit = data.apply(lambda x: int(str(x)[2]))
+    
+    # Calculate the frequency of each digit
+    freq_1 = len(first_digit[first_digit == 1])/len(first_digit)
+    freq_2 = len(second_digit[second_digit == 2])/len(second_digit)
+    freq_3 = len(third_digit[third_digit == 3])/len(third_digit)
+    
+    return freq_1, freq_2, freq_3
 
-def analyze_data(data, column):
-    first_digits = [int(str(i)[0]) for i in data[column] if i > 0]
-    second_digits = [int(str(i)[1]) for i in data[column] if i > 0 and len(str(i)) >= 2]
-    third_digits = [int(str(i)[2]) for i in data[column] if i > 0 and len(str(i)) >= 3]
-
-    results = {
-        "First Digit": detect(first_digits),
-        "Second Digit": detect(second_digits),
-        "Third Digit": detect(third_digits),
-    }
-
-    return results
-
-st.title("Fraud Detection using Newcomb-Benford's Law")
-st.write("This app uses Newcomb-Benford's Law to detect fraud in data.")
-
-# Load data
-df = load_data()
-if df is not None:
-    st.write("Data Preview:")
+# Function to load the Excel file and apply the frequency calculation function
+def load_data(file):
+    df = pd.read_excel(file)
     st.write(df.head())
 
-    # Select column to analyze
-    columns = list(df.columns)
-    column = st.selectbox("Select column to analyze:", options=columns)
+    # Get the column with the numbers to be analyzed
+    column_name = st.selectbox("Select the column with the numbers", options=df.columns)
+    data = df[column_name]
 
-    # Analyze data
-    results = analyze_data(df, column)
+    # Calculate the frequency of the first, second, and third digits
+    freq_1, freq_2, freq_3 = calc_freq(data)
 
-    # Display results
-    st.write("Results:")
-    st.write(results)
+    # Display the frequencies
+    st.write(f"Frequency of first digit: {freq_1:.2f}")
+    st.write(f"Frequency of second digit: {freq_2:.2f}")
+    st.write(f"Frequency of third digit: {freq_3:.2f}")
 
-    # Display chart
-    st.write("Chart:")
-    chart_data = pd.DataFrame.from_dict(results, orient="index")
-    st.bar_chart(chart_data)
+    # Create a chart of the frequencies
+    fig, ax = plt.subplots()
+    ax.bar(['First digit', 'Second digit', 'Third digit'], [freq_1, freq_2, freq_3])
+    ax.set_ylabel('Frequency')
+    st.pyplot(fig)
+
+# Set up the Streamlit app
+st.set_page_config(page_title="Fraud Detection with Newcomb-Benford's Law", page_icon=":money_with_wings:")
+
+st.write("# Fraud Detection with Newcomb-Benford's Law")
+
+# Allow the user to upload an Excel file
+uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx"])
+
+# Load the data and display the frequencies and chart
+if uploaded_file is not None:
+    load_data(uploaded_file)
+else:
+    st.warning("Please upload a file.")
