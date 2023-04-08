24b69fce5ff3028e89db3e2aad7e3319833607a9
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-08 15:57:59 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -66,7 +66,7 @@ if uploaded_file is not None:
     column = st.selectbox("Select Column",df.columns)
 
     st.write('**Data sample:**')
-    st.write(column.head())
+    st.dataframe(column.head())
 
     st.write('**Analysis:**')
     result = analyze_data(column)
