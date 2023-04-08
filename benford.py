4a94bd5deaa1693aa78e209c779798aa573a66b6
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-08 17:02:47 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -1,72 +1,38 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import math
 from scipy.stats import chisquare
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def calculate_expected_frequencies(df, column_name, digit_position):
-    data = df[column_name]
-    # Get the digit at the specified position for each value
-    digits = data.apply(lambda x: str(x)[digit_position-1] if len(str(x)) >= digit_position else np.nan)
-    digits = digits.dropna().astype(int)
-    # Calculate the expected frequency for each digit based on the position
-    if digit_position == 1:
-        expected_freq = np.log10(1 + 1/np.arange(1, 10))
-    elif digit_position == 2:
-        expected_freq = np.log10(1 + 1/np.arange(10, 100))
-        expected_freq = np.round(expected_freq / np.sum(expected_freq), 4)
-        expected_freq_dict = dict(zip(range(10, 100), expected_freq))
-    elif digit_position == 3:
-        expected_freq = np.log10(1 + 1/np.arange(100, 1000))
-        expected_freq = np.round(expected_freq / np.sum(expected_freq), 4)
-        expected_freq_dict = dict(zip(range(100, 1000), expected_freq))
-    # Convert the dictionary to a dataframe and rename the index column
-    freq_df = pd.DataFrame.from_dict(expected_freq_dict, orient='index', columns=['Expected Frequency'])
-    freq_df.index.name = 'Digit'
-    # Calculate the actual frequency for each digit
-    actual_freq_dict = dict(digits.value_counts(normalize=True))
-    # Merge the expected and actual frequencies into a single dataframe
-    freq_df = freq_df.merge(pd.DataFrame({'Digit': range(1, 10),
-                                           'Actual Frequency': [actual_freq_dict.get(d, 0) for d in range(1, 10)]}),
-                             on='Digit')
-    return freq_df
+# set page title and icon
+st.set_page_config(page_title='Newcomb Benford Law', page_icon=':bar_chart:', layout='wide')
 
-def plot_frequencies(df):
-    fig, ax = plt.subplots()
-    ax.bar(df.index, df['Actual Frequency'], label='Actual')
-    ax.plot(df.index, df['Expected Frequency'], color='red', label='Expected')
-    ax.legend()
-    ax.set_xlabel('Digit')
-    ax.set_ylabel('Frequency')
-    ax.set_title('Newcomb-Benford Law')
-    st.pyplot(fig)
+# page header
+st.write('# Newcomb Benford Law')
 
-# Upload a file and get the column name
-uploaded_file = st.file_uploader('Upload a file', type=['xlsx'])
-if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file)
-    st.write('### Data Preview')
-    st.write(df.head())
-    column_name = st.selectbox('Select a column', options=df.columns)
-    st.write(f'### Column: {column_name}')
+# file upload and column selection
+file = st.file_uploader('Upload Excel file', type=['xlsx', 'xls'])
+if file is not None:
+    df = pd.read_excel(file)
+    column = st.selectbox('Select a column to analyze', df.columns)
 
-    # Calculate the expected and actual frequencies for the first, second, and third digits
-    st.write('### First Digit Analysis')
-    first_digit_df = calculate_expected_frequencies(df, column_name, 1)
-    st.write(first_digit_df)
-    plot_frequencies(first_digit_df)
+    # first digit analysis
+    st.write('## First Digit Analysis')
+    digit_position = 'First'
+    data = df[column].astype(str).str[0].astype(int)
+    first_digit_df = pd.DataFrame({'Digit': range(1, 10)})
+    first_digit_df['Actual Frequency'] = data.value_counts(normalize=True).sort_index().values
+    first_digit_df['Expected Frequency'] = [math.log10(1 + 1/d) for d in range(1, 10)]
 
-    st.write('### Second Digit Analysis')
-    second_digit_df = calculate_expected_frequencies(df, column_name, 2)
-    st.write(second_digit_df)
-    plot_frequencies(second_digit_df)
+    # update expected frequency dictionary based on position
+    expected_freq_dict = {d: math.log10(1 + 1/d) for d in range(1, 10)}
+    if digit_position == 'Second':
+        expected_freq_dict = {d: 2 * freq for d, freq in expected_freq_dict.items()}
+    elif digit_position == 'Third':
+        expected_freq_dict = {d: 3 * freq for d, freq in expected_freq_dict.items()}
 
-    st.write('### Third Digit Analysis')
-    third_digit_df = calculate_expected_frequencies(df, column_name, 3)
-    st.write(third_digit_df)
-    plot_frequencies(third_digit_df)
-
-    # Perform the chi-square test on the first digit analysis
+    # chi-square test
     st.write('### Chi-Square Test')
     st.write('Null Hypothesis: The observed and expected frequencies are not significantly different.')
     st.write('Alternative Hypothesis: The observed and expected frequencies are significantly different.')
@@ -78,3 +44,40 @@ if uploaded_file is not None:
         st.write('Reject the null hypothesis.')
     else:
         st.write('Fail to reject the null hypothesis.')
+
+    # chart
+    fig, ax = plt.subplots()
+    sns.barplot(x='Digit', y='Frequency', data=pd.DataFrame({'Digit': range(1, 10), 'Frequency': first_digit_df['Actual Frequency']}))
+    sns.lineplot(x='Digit', y='Expected Frequency', data=first_digit_df, color='red', marker='o')
+    ax.set(xlabel='Digit', ylabel='Frequency', title=f'First Digit Analysis ({column})')
+    st.pyplot(fig)
+
+    # second digit analysis
+    st.write('## Second Digit Analysis')
+    digit_position = 'Second'
+    data = df[column].astype(str).str[1].astype(int)
+    second_digit_df = pd.DataFrame({'Digit': range(0, 10)})
+    second_digit_df['Actual Frequency'] = data.value_counts(normalize=True).sort_index().values
+    second_digit_df['Expected Frequency'] = [2 * freq for d, freq in expected_freq_dict.items()]
+
+    # chart
+    fig, ax = plt.subplots()
+    sns.barplot(x='Digit', y='Frequency', data=pd.DataFrame({'Digit': range(0, 10), 'Frequency': second_digit_df['Actual Frequency']}))
+    sns.lineplot(x='Digit', y='Expected Frequency', data=second_digit_df, color='red', marker='o')
+    ax.set(xlabel='Digit', ylabel='Frequency', title=f'Second Digit Analysis ({column})')
+    st.pyplot(fig)
+
+    # third digit analysis
+    st.write('## Third Digit Analysis')
+    digit_position = 'Third'
+    data = df[column].astype(str).str[2].astype(int)
+    third_digit_df = pd.DataFrame({'Digit': range(0, 10)})
+    third_digit_df['Actual Frequency'] = data.value_counts(normalize=True).sort_index().values
+    third_digit_df['Expected Frequency'] = [3 * freq for d, freq in expected_freq_dict.items()]
+
+    # chart
+    fig, ax = plt.subplots()
+    sns.barplot(x='Digit', y='Frequency', data=pd.DataFrame({'Digit': range(0, 10), 'Frequency': third_digit_df['Actual Frequency']}))
+    sns.lineplot(x='Digit', y='Expected Frequency', data=third_digit_df, color='red', marker='o')
+    ax.set(xlabel='Digit', ylabel='Frequency', title=f'Third Digit Analysis ({column})')
+    st.pyplot(fig)
