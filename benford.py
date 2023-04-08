dfc3d6d444fd14fab92df204e6a7327f67976c06
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-08 15:10:38 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -1,98 +1,73 @@
-import streamlit as st
 import pandas as pd
 import numpy as np
 from scipy.stats import chisquare
+import streamlit as st
 
-# Define a function to calculate the first digit of a number
-def first_digit(n):
-    return int(str(n)[0])
+def check_fraud(numbers):
+    # Get the first, second, and third digit of each number
+    first_digits = np.array([int(str(n)[0]) for n in numbers])
+    second_digits = np.array([int(str(n)[1]) for n in numbers if len(str(n)) > 1])
+    third_digits = np.array([int(str(n)[2]) for n in numbers if len(str(n)) > 2])
 
-# Define a function to calculate the second digit of a number
-def second_digit(n):
-    return int(str(n)[1])
+    # Get the expected frequencies according to Newcomb-Benford's law
+    expected_frequencies = np.array([np.log10(1 + 1 / d) for d in range(1, 10)])
+    expected_frequencies *= len(first_digits)
 
-# Define a function to calculate the third digit of a number
-def third_digit(n):
-    return int(str(n)[2])
+    # Get the observed frequencies for the first digit
+    observed_frequencies, _ = np.histogram(first_digits, bins=np.arange(1, 11))
 
-# Define a function to apply the Newcomb-Benford Law to a series of numbers
-def apply_newcomb_benford_law(data, digit):
-    data = pd.Series(data)[1:]
-    # Calculate the frequency of each digit
-    digit_counts = data.apply(digit).value_counts(normalize=True)
-    # Calculate the expected frequency based on Newcomb-Benford Law
-    expected_counts = np.log10(1 + 1 / np.arange(1, 10))
-    expected_counts *= data.shape[0]
-    # Calculate the chi-square statistic
-    chi_square = chisquare(digit_counts, expected_counts)
-    # Return the digit frequencies and the chi-square statistic
-    return digit_counts, chi_square
+    # Test if the observed frequencies for the first digit follow the expected frequencies using a chi-square test
+    _, p_value = chisquare(observed_frequencies, expected_frequencies)
 
-# Define the Streamlit app
-st.set_page_config(page_title="Newcomb Benford's Law Fraud Detection", page_icon=":guardsman:", layout="wide")
-st.title("Newcomb Benford's Law Fraud Detection")
-st.write("This app analyzes fraud using Newcomb Benford's Law on the first, second, and third digits of numbers in a dataset.")
+    # If the p-value is less than the significance level, assume fraud
+    if p_value < 0.05:
+        return True
+    
+    # Get the observed frequencies for the second digit
+    observed_frequencies, _ = np.histogram(second_digits, bins=np.arange(1, 11))
 
-# Upload a dataset
-st.sidebar.title("Upload a Dataset")
-uploaded_file = st.sidebar.file_uploader(label="Choose a CSV or Excel file", type=["csv", "xlsx"])
+    # Test if the observed frequencies for the second digit follow the expected frequencies using a chi-square test
+    _, p_value = chisquare(observed_frequencies, expected_frequencies)
+
+    # If the p-value is less than the significance level, assume fraud
+    if p_value < 0.05:
+        return True
+    
+    # Get the observed frequencies for the third digit
+    observed_frequencies, _ = np.histogram(third_digits, bins=np.arange(1, 11))
+
+    # Test if the observed frequencies for the third digit follow the expected frequencies using a chi-square test
+    _, p_value = chisquare(observed_frequencies, expected_frequencies)
+
+    # If the p-value is less than the significance level, assume fraud
+    if p_value < 0.05:
+        return True
+
+    return False
+
+# Set page title
+st.set_page_config(page_title="Fraud Detection App")
+
+# Add title and subtitle
+st.title("Fraud Detection App")
+st.write("This app detects fraud based on the Newcomb-Benford's law applied to the first, second, and third digits of a number in an Excel file.")
+
+# Allow user to upload Excel file
+uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls"])
+
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file) if uploaded_file.type == "text/csv" else pd.read_excel(uploaded_file, engine="openpyxl")
-    st.sidebar.write("Dataset Summary:")
-    st.sidebar.write(df.describe())
-    df_column = st.sidebar.selectbox('Select the Column to evaluate:', df.columns)
-    
-    # Analyze the first digit
-    st.sidebar.title("First Digit Analysis")
-    first_digit_counts, first_digit_chi_square = apply_newcomb_benford_law(df_column, first_digit)
-    st.sidebar.write("First Digit Counts:")
-    st.sidebar.write(first_digit_counts)
-    st.sidebar.write("Chi-Square Statistic:")
-    st.sidebar.write(first_digit_chi_square.statistic)
+    # Load the Excel file
+    df = pd.read_excel(uploaded_file)
 
-    # Analyze the second digit
-    st.sidebar.title("Second Digit Analysis")
-    second_digit_counts, second_digit_chi_square = apply_newcomb_benford_law(df_column, second_digit)
-    st.sidebar.write("Second Digit Counts:")
-    st.sidebar.write(second_digit_counts)
-    st.sidebar.write("Chi-Square Statistic:")
-    st.sidebar.write(second_digit_chi_square.statistic)
+    # Show the user the available columns in the Excel file
+    available_columns = list(df.columns)
+    column_to_check = st.selectbox("Select a column to check for fraud", available_columns)
 
-    # Analyze the third digit
-    st.sidebar.title("Third Digit Analysis")
-    third_digit_counts, third_digit_chi_square = apply_newcomb_benford_law(df_column, third_digit)
-    st.sidebar.write("Third Digit Counts:")
-    st.sidebar.write(third_digit_counts)
-    st.sidebar.write("Chi-Square Statistic:")
-    st.sidebar.write(third_digit_chi_square.statistic)
-
-    # Display the dataset and digit frequencies
-    st.write("Dataset:")
-    st.write(df)
-    st.write("Digit Frequencies:")
-    st.write(pd.DataFrame({
-        "First Digit": first_digit_counts,
-        "Second Digit": second_digit_counts,
-        "Third Digit": third_digit_counts}))
-    
-    import plotly.express as px
-
-    # Add a bar chart of the digit frequencies
-    fig1 = px.bar(pd.DataFrame({
-        "Digit": range(1, 10),
-        "First Digit": first_digit_counts,
-        "Second Digit": second_digit_counts,
-        "Third Digit": third_digit_counts
-    }).melt(id_vars="Digit", var_name="Digit Position", value_name="Frequency"), 
-                 x="Digit", y="Frequency", color="Digit Position", barmode="group", 
-                 title="Digit Frequencies")
-    st.plotly_chart(fig1)
-
-    # Add a pie chart of the chi-square statistics
-    fig2 = px.pie(pd.DataFrame({
-        "Chi-Square Statistic": [first_digit_chi_square.statistic, second_digit_chi_square.statistic, third_digit_chi_square.statistic],
-        "Digit Position": ["First Digit", "Second Digit", "Third Digit"]
-    }), values="Chi-Square Statistic", names="Digit Position", 
-                 title="Chi-Square Statistics")
-    st.plotly_chart(fig2)
-
+    # Show a message while the fraud detection is running
+    with st.spinner("Detecting fraud..."):
+        # Apply the check_fraud function to the selected column
+        fraud_mask = df[column_to_check].apply(lambda x: check_fraud(x))
+        
+    # Show the user the results of the fraud detection
+    st.write("Fraud detected in the following rows:")
+    st.write(df[fraud_mask])
