3eff73859d1e1e544764d6f7fd36993847660a4b
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-08 16:48:47 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -2,17 +2,17 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-
-def calculate_expected_frequencies(df, column_name):
+def calculate_expected_frequencies(df, column_name, digit_position):
     data = df[column_name]
-    # Get the first digit of each value
-    first_digits = data.apply(lambda x: int(str(x)[0]))
+    # Get the digit at the specified position for each value
+    digits = data.apply(lambda x: str(x)[digit_position-1] if len(str(x)) >= digit_position else np.nan)
+    digits = digits.dropna().astype(int)
     # Calculate the expected frequency for each digit
     expected_freq = np.log10(1 + 1/np.arange(1, 10))
     expected_freq = np.round(expected_freq / np.sum(expected_freq), 4)
     expected_freq_dict = dict(zip(range(1, 10), expected_freq))
     # Calculate the actual frequency for each digit
-    actual_freq_dict = dict(first_digits.value_counts(normalize=True))
+    actual_freq_dict = dict(digits.value_counts(normalize=True))
     # Merge the expected and actual frequencies into a single dataframe
     freq_df = pd.DataFrame({'Digit': range(1, 10),
                             'Expected Frequency': expected_freq,
@@ -27,14 +27,15 @@ uploaded_file = st.file_uploader('Upload an Excel file', type=['xlsx', 'xls'])
 if uploaded_file is not None:
     # Read the data from the uploaded file into a pandas dataframe
     df = pd.read_excel(uploaded_file)
-    # Ask the user to select a column from the dataframe
+    # Ask the user to select a column and a digit position from the dataframe
     column_name = st.selectbox('Select a column', options=list(df.columns))
-    # Calculate the expected and actual frequencies for each digit
-    freq_df = calculate_expected_frequencies(df, column_name)
+    digit_position = st.selectbox('Select a digit position', options=[2, 3])
+    # Calculate the expected and actual frequencies for the specified digit position
+    freq_df = calculate_expected_frequencies(df, column_name, digit_position)
     # Plot a bar chart comparing the expected and actual frequencies
     fig, ax = plt.subplots()
     freq_df.plot(x='Digit', y=['Expected Frequency', 'Actual Frequency'], kind='bar', ax=ax)
-    ax.set_xlabel('Digit')
+    ax.set_xlabel(f'{digit_position}nd/rd Digit')
     ax.set_ylabel('Frequency')
-    ax.set_title(f'Newcomb Benford\'s Law for {column_name}')
+    ax.set_title(f'Newcomb Benford\'s Law for {column_name} ({digit_position}nd/rd Digit)')
     st.pyplot(fig)
