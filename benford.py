d00a6a77260522e5650356203a3d16e605fde746
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-08 16:45:11 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -1,82 +1,35 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Function to calculate the frequency of the first, second, and third digits
-def calc_freq(data):
-    # Get the first digit of each number
-    first_digit = data.apply(lambda x: int(str(x)[0]) if str(x)[0].isdigit() else None)
-    # Get the second digit of each number
-    second_digit = data.apply(lambda x: int(str(x)[1]) if str(x)[1].isdigit() else None)
-    # Get the third digit of each number
-    third_digit = data.apply(lambda x: int(str(x)[2]) if str(x)[2].isdigit() else None)
-    
-    # Calculate the frequency of each digit
-    freq_1 = len(first_digit[first_digit == 1])/len(first_digit.dropna())
-    freq_2 = len(second_digit[second_digit == 2])/len(second_digit.dropna())
-    freq_3 = len(third_digit[third_digit == 3])/len(third_digit.dropna())
-    
-    return freq_1, freq_2, freq_3
-
-
-# Function to load the Excel file and apply the frequency calculation function
-def load_data(file):
-    df = pd.read_excel(file)
-    st.write(df.head())
-
-    # Get the column with the numbers to be analyzed
-    column_name = st.selectbox("Select the column with the numbers", options=df.columns)
+def calculate_expected_frequencies(df, column_name):
     data = df[column_name]
+    # Get the first digit of each value
+    first_digits = data.apply(lambda x: int(str(x)[0]))
+    # Calculate the expected frequency for each digit
+    expected_freq = np.log10(1 + 1/np.arange(1, 10))
+    expected_freq = np.round(expected_freq / np.sum(expected_freq), 4)
+    expected_freq_dict = dict(zip(range(1, 10), expected_freq))
+    # Calculate the actual frequency for each digit
+    actual_freq_dict = dict(first_digits.value_counts(normalize=True))
+    # Merge the expected and actual frequencies into a single dataframe
+    freq_df = pd.DataFrame({'Digit': range(1, 10),
+                            'Expected Frequency': expected_freq,
+                            'Actual Frequency': [actual_freq_dict.get(d, 0) for d in range(1, 10)]})
+    return freq_df
 
-    # Calculate the frequency of the first, second, and third digits
-    freq_1, freq_2, freq_3 = calc_freq(data)
+st.title('Newcomb Benford\'s Law Anomaly Detection')
 
-    # Display the frequencies
-    st.write(f"Frequency of first digit: {freq_1:.2f}")
-    st.write(f"Frequency of second digit: {freq_2:.2f}")
-    st.write(f"Frequency of third digit: {freq_3:.2f}")
+# Ask the user to upload an excel file
+uploaded_file = st.file_uploader('Upload an Excel file', type=['xlsx', 'xls'])
 
-    # Create a chart of the frequencies
+if uploaded_file is not None:
+    # Read the data from the uploaded file into a pandas dataframe
+    df = pd.read_excel(uploaded_file)
+    # Ask the user to select a column from the dataframe
+    column_name = st.selectbox('Select a column', options=list(df.columns))
+    # Calculate the expected and actual frequencies for each digit
+    freq_df = calculate_expected_frequencies(df, column_name)
+    # Plot a bar chart comparing the expected and actual frequencies
     fig, ax = plt.subplots()
-    ax.bar(['First digit', 'Second digit', 'Third digit'], [freq_1, freq_2, freq_3])
+    freq_df.plot(x='Digit', y=['Expected Frequency', 'Actual Frequency'], kind='bar', ax=ax)
+    ax.set_xlabel('Digit')
     ax.set_ylabel('Frequency')
+    ax.set_title(f'Newcomb Benford\'s Law for {column_name}')
     st.pyplot(fig)
-
-    
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Function to calculate the frequency of each digit from 0 to 9
-def calc_freq(data):
-    freqs = []
-    for i in range(10):
-        freq = len(data[data.astype(str).str.startswith(str(i))])/len(data)
-        freqs.append(freq)
-    return freqs
-
-# Define the Streamlit app
-# Add a title to the app
-st.title("Newcomb-Benford's Law Fraud Detection")
-
-# Upload an Excel file and select a column to analyze
-file = st.file_uploader("Upload an Excel file", type=["xls", "xlsx"])
-if file is not None:
-    df = pd.read_excel(file)
-    column = st.selectbox("Select a column to analyze", df.columns.tolist())
-
-    # Calculate the frequencies of each digit from 0 to 9 using Newcomb-Benford's Law
-    freqs = calc_freq(df[column])
-
-    # Create a bar chart for each digit to compare the observed frequencies with the expected ones
-    for i in range(10):
-        fig, ax = plt.subplots()
-        ax.bar([0, 1], [freqs[i], 0.1], label=["Observed", "Expected"])
-        ax.set_xlabel("Frequency")
-        ax.set_ylabel("Digit " + str(i))
-        ax.set_title("Newcomb-Benford's Law Frequencies")
-        ax.set_xticks([0, 1])
-        ax.set_xticklabels(["Observed", "Expected"])
-        ax.set_ylim([0, 0.5])
-        st.pyplot(fig)
