b27d1fec9f3cf5dd4a2c11a75dc5488b85def787
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-08 22:09:28 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -17,19 +17,25 @@ def chi_square_test(df, column):
     else:
         st.write("The column", column, "does not seem to contain fraudulent data (p-value =", p, ")")
 
+import pandas as pd
+import numpy as np
+import streamlit as st
+import matplotlib.pyplot as plt
+from scipy.stats import chisquare
+
 def benfords_law_test(df, column):
-    observed_values = df[column].astype(str).str[0].value_counts().sort_index()
-    expected_values = pd.Series([np.log10(1 + 1 / i) for i in range(1, 10)], index=[str(i) for i in range(1, 10)]) * len(df[column])
-    test_statistic, p_value = kstest(observed_values, expected_values)
-    if p_value < 0.05:
-        st.write("The column", column, "may contain fraudulent data (p-value =", p_value, ")")
-    else:
-        st.write("The column", column, "does not seem to contain fraudulent data (p-value =", p_value, ")")
+    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
 
-    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+    if df[column].dtype != 'int64' and df[column].dtype != 'float64':
+        st.write("The selected column is not numeric.")
+        return
 
-    # First position
-    observed_values_1 = df[column].astype(str).str[0].value_counts().sort_index()
+    first_digit_values = df[column].astype(str).str[0]
+    second_digit_values = df[column].astype(str).str[1]
+    third_digit_values = df[column].astype(str).str[2]
+
+    st.write("### First Digit")
+    observed_values_1 = first_digit_values.value_counts().sort_index()
     expected_values_1 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 10)], index=[str(i) for i in range(1, 10)]) * len(df[column])
     axs[0].bar(observed_values_1.index, observed_values_1.values / len(df[column]), label='Observed')
     axs[0].plot(expected_values_1.index, expected_values_1.values / len(df[column]), 'ro-', label='Expected')
@@ -38,19 +44,19 @@ def benfords_law_test(df, column):
     axs[0].legend()
     axs[0].set_title('First position')
 
-    # Second position
-    observed_values_2 = df[column].astype(str).str[1].value_counts().sort_index()
-    expected_values_2 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 10)], index=[str(i) for i in range(1, 10)]) * len(df[column])
+    st.write("### Second Digit")
+    observed_values_2 = second_digit_values.value_counts().sort_index()
+    expected_values_2 = pd.Series([np.log10(1 + 1 / (10 * i + j)) for i in range(9) for j in range(10)], index=[str(i) for i in range(10)]) * len(df[column])
     axs[1].bar(observed_values_2.index, observed_values_2.values / len(df[column]), label='Observed')
     axs[1].plot(expected_values_2.index, expected_values_2.values / len(df[column]), 'ro-', label='Expected')
     axs[1].set_xlabel('Second digit')
     axs[1].set_ylabel('Frequency')
     axs[1].legend()
     axs[1].set_title('Second position')
-        
-    # Third position
-    observed_values_3 = df[column].astype(str).str[2].value_counts().sort_index()
-    expected_values_3 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 10)], index=[str(i) for i in range(1, 10)]) * len(df[column])
+
+    st.write("### Third Digit")
+    observed_values_3 = third_digit_values.value_counts().sort_index()
+    expected_values_3 = pd.Series([np.log10(1 + 1 / (100 * i + 10 * j + k)) for i in range(9) for j in range(10) for k in range(10)], index=[str(i) for i in range(10)]) * len(df[column])
     axs[2].bar(observed_values_3.index, observed_values_3.values / len(df[column]), label='Observed')
     axs[2].plot(expected_values_3.index, expected_values_3.values / len(df[column]), 'ro-', label='Expected')
     axs[2].set_xlabel('Third digit')
@@ -60,8 +66,6 @@ def benfords_law_test(df, column):
 
     plt.tight_layout()
     st.pyplot(fig)
-
-
 
 st.title("## Benford's Law and Chi-Square Test")
 
