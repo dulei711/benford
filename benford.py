5e9b5b893f78f99ebf0d795bb5c4f79203407940
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-08 21:52:47 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy.stats import chi2_contingency
 from scipy.stats import kstest
 
+st.title("fraud suspicion tests")
+
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
