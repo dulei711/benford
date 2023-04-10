d7b3fe5c0fb9ebf1bc466ed4a320937958eae233
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-10 17:26:23 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.stats import chisquare
+import plotly.graph_objects as go
+import plotly.io as pio
 
 def benfords_law_test(df, column):    
     # calculate the expected frequencies for the first digit using Benford's Law
@@ -17,34 +18,25 @@ def benfords_law_test(df, column):
     first_digit_freq_obs = first_digit_counts / first_digit_counts.sum()
     two_digit_freq_obs = two_digit_counts / two_digit_counts.sum()
     three_digit_freq_obs = three_digit_counts / three_digit_counts.sum()
-    # create the figure and subplots
-    fig, axs = plt.subplots(3, 1, figsize=(20, 10))
-    # plot the first digit data
-    axs[0].bar(range(1, 10), first_digit_freq, alpha=0.5, label='Expected Frequency')
-    axs[0].bar(first_digit_counts.index.astype(int), first_digit_freq_obs, alpha=0.5, label='Observed Frequency')
-    axs[0].set_xlabel('First Digit')
-    axs[0].set_ylabel('Frequency')
-    axs[0].legend()
-    axs[0].set_title('Benford\'s Law Analysis: First Digit')
-    # plot the two digit data
-    axs[1].bar(range(10, 100), two_digit_freq, alpha=0.5, label='Expected Frequency')
-    axs[1].bar(two_digit_counts.index.astype(int), two_digit_freq_obs, alpha=0.5, label='Observed Frequency')
-    axs[1].set_xlabel('Two Digits')
-    axs[1].set_ylabel('Frequency')
-    axs[1].legend()
-    axs[1].set_title('Benford\'s Law Analysis: Two Digits')
-    # plot the three digit data
-    axs[2].bar(range(100, 1000), three_digit_freq, alpha=0.5, label='Expected Frequency')
-    axs[2].bar(three_digit_counts.index.astype(int), three_digit_freq_obs, alpha=0.5, label='Observed Frequency')
-    axs[2].set_xlabel('Three Digits')
-    axs[2].set_ylabel('Frequency')
-    axs[2].legend()
-    axs[2].set_title('Benford\'s Law Analysis: Three Digits')
-    # set the overall title and layout
-    plt.suptitle('Benford\'s Law Analysis')
-    plt.tight_layout()
-    # display the plot
-    st.pyplot(fig)
+    
+    # create subplots
+    fig = make_subplots(rows=3, cols=1, subplot_titles=("Benford's Law Analysis: First Digit", "Benford's Law Analysis: Two Digits", "Benford's Law Analysis: Three Digits"), shared_xaxes=True)
+    # add bar traces for first digit
+    fig.add_trace(go.Bar(x=range(1, 10), y=first_digit_freq, name='Expected Frequency', marker=dict(opacity=0.5)), row=1, col=1)
+    fig.add_trace(go.Bar(x=first_digit_counts.index.astype(int), y=first_digit_freq_obs, name='Observed Frequency', marker=dict(opacity=0.5)), row=1, col=1)
+    fig.update_xaxes(title_text="First Digit", row=1, col=1)
+    fig.update_yaxes(title_text="Frequency", row=1, col=1)    
+    # add bar traces for two digits
+    fig.add_trace(go.Bar(x=range(10, 100), y=two_digit_freq, name='Expected Frequency', marker=dict(opacity=0.5)), row=2, col=1)
+    fig.add_trace(go.Bar(x=two_digit_counts.index.astype(int), y=two_digit_freq_obs, name='Observed Frequency', marker=dict(opacity=0.5)), row=2, col=1)
+    fig.update_xaxes(title_text="Two Digits", row=2, col=1)
+    fig.update_yaxes(title_text="Frequency", row=2, col=1)    
+    # add bar traces for three digits
+    fig.add_trace(go.Bar(x=range(100, 1000), y=three_digit_freq, name='Expected Frequency', marker=dict(opacity=0.5)), row=3, col=1)
+    fig.add_trace(go.Bar(x=three_digit_counts.index.astype(int), y=three_digit_freq_obs, name='Observed Frequency', marker=dict(opacity=0.5)), row=3, col=1)
+    fig.update_xaxes(title_text="Three Digits", row=3, col=1)
+    fig.update_yaxes(title_text="Frequency", row=3, col=1)
+    st.plotly_chart(fig, use_container_width=True)
 
 st.title("## Benford's Law Test")
 
