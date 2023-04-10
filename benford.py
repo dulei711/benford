e94d3bb8acdcff0ee57187333715013a0759bd98
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-10 16:16:09 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -1,4 +1,4 @@
-import streamlit as st
+'''import streamlit as st
 import pandas as pd
 import numpy as np
 from scipy.stats import chi2_contingency
@@ -30,6 +30,40 @@ def benfords_law_test(df, column):
     expected_values_3 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 1000)], index=[str(i) for i in range(0, 999)]) * len(df[column])                    
     p_value(observed_values_3, expected_values_3, "third")
     st.table(observed_values_3)
+'''
+
+import pandas as pd
+import numpy as np
+import streamlit as st
+from scipy.stats import chisquare
+
+def benfords_law_test(df, column):    
+    # First position
+    observed_values_1 = df[column].astype(str).str[0].value_counts().sort_index()
+    expected_values_1 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 10)], index=[str(i) for i in range(1, 10)]) * len(df[column])
+    p_value_1 = chisquare(observed_values_1, expected_values_1)[1]
+    
+    # Second position
+    observed_values_2 = df[column].astype(str).str[:2].value_counts().sort_index()
+    expected_values_2 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 100)], index=[str(i).zfill(2) for i in range(1, 100)]) * len(df[column])
+    p_value_2 = chisquare(observed_values_2, expected_values_2)[1]
+    
+    # Third position
+    observed_values_3 = df[column].astype(str).str[:3].value_counts().sort_index()
+    expected_values_3 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 1000)], index=[str(i).zfill(3) for i in range(1, 1000)]) * len(df[column])
+    p_value_3 = chisquare(observed_values_3, expected_values_3)[1]
+    
+    # Create a dataframe for the comparison of observed and expected frequencies
+    df_comparison = pd.DataFrame({
+        'Digit': [f'{i}' for i in range(1,10)] + [f'{i:02}' for i in range(10,100)] + [f'{i:03}' for i in range(100,1000)],
+        'Observed Frequency': list(observed_values_1.values) + list(observed_values_2.values) + list(observed_values_3.values),
+        'Expected Frequency': list(expected_values_1.values) + list(expected_values_2.values) + list(expected_values_3.values)
+    })
+    
+    # Add a column for the p-values
+    df_comparison['p-value'] = [p_value_1] * 9 + [p_value_2] * 90 + [p_value_3] * 900
+    
+    return df_comparison
 
     
 st.title("## Benford's Law Test")
