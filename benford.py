74c1a133a32dfa729b622bde23c3a4f567a424c0
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-10 14:48:10 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -5,12 +5,12 @@ from scipy.stats import chi2_contingency
 from scipy.stats import kstest
 import matplotlib.pyplot as plt
 
-def p_value(observed_values, expected_values):
+def p_value(observed_values, expected_values, digit):
     test_statistic, p_value = kstest(observed_values, expected_values)
     if p_value < 0.05:
-        st.write("The column", column, "may contain fraudulent data (p-value =", p_value, ")")
+        st.write("The column", column, "may contain fraudulent data (p-value =", p_value, ") on the ", digit, "digit")
     else:
-        st.write("The column", column, "does not seem to contain fraudulent data (p-value =", p_value, ")")
+        st.write("The column", column, "does not seem to contain fraudulent data (p-value =", p_value, ") on the ", digit, "digit")
 
 def benfords_law_test(df, column):
     fig, axs = plt.subplots(3, 1, figsize=(20, 10))
@@ -18,7 +18,7 @@ def benfords_law_test(df, column):
     # First position
     observed_values_1 = df[column].astype(str).str[0].value_counts().sort_index()
     expected_values_1 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 10)], index=[str(i) for i in range(1, 10)]) * len(df[column])
-    p_value(observed_values_1, expected_values_1)
+    p_value(observed_values_1, expected_values_1, "first")
     axs[0].bar(observed_values_1.index, observed_values_1.values / len(df[column]), label='Observed')
     axs[0].plot(expected_values_1.index, expected_values_1.values / len(df[column]), 'ro-', label='Expected')
     axs[0].set_xlabel('First digit')
@@ -29,7 +29,7 @@ def benfords_law_test(df, column):
     # Second position
     observed_values_2 = df[column].astype(str).str[:2].value_counts().sort_values()
     expected_values_2 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 100)], index=[str(i) for i in range(0, 99)]) * len(df[column])
-    p_value(observed_values_2, expected_values_2)
+    p_value(observed_values_2, expected_values_2, "second")
     axs[1].bar(observed_values_2.index, observed_values_2.values / len(df[column]), label='Observed')
     axs[1].plot(expected_values_2.index, expected_values_2.values / len(df[column]), 'ro-', label='Expected')
     axs[1].set_xlabel('Second digit')
@@ -40,7 +40,7 @@ def benfords_law_test(df, column):
     # Third position
     observed_values_3 = df[column].astype(str).str[:3].value_counts().sort_values()
     expected_values_3 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 1000)], index=[str(i) for i in range(0, 999)]) * len(df[column])
-    p_value(observed_values_3, expected_values_3)
+    p_value(observed_values_3, expected_values_3, "third")
     axs[2].bar(observed_values_3.index, observed_values_3.values / len(df[column]), label='Observed')
     axs[2].plot(expected_values_3.index, expected_values_3.values / len(df[column]), 'ro-', label='Expected')
     axs[2].set_xlabel('Third digit')
