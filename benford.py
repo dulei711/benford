d0f9235cab2beb2ea05b2ddde3e3e42cd0630a9c
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-10 15:36:05 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -12,7 +12,7 @@ def p_value(observed_values, expected_values, digit):
     else:
         st.write("The column", column, "does not seem to contain fraudulent data (p-value =", p_value, ") on the ", digit, "digit")
 
-def benfords_law_test(df, column):
+'''def benfords_law_test(df, column):
     fig, axs = plt.subplots(3, 1, figsize=(20, 10))
     
     # First position
@@ -73,7 +73,47 @@ def benfords_law_test(df, column):
     plt.subplots_adjust(hspace=0.4)
     plt.tight_layout()
     st.pyplot(fig)
-       
+'''
+
+def benfords_law_test(df, column):
+    fig, axs = plt.subplots(3, 1, figsize=(20, 10))
+    
+    # First position
+    observed_values_1 = df[column].astype(str).str[0].value_counts().sort_index()
+    expected_values_1 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 10)], index=[str(i) for i in range(1, 10)]) * len(df[column])
+    p_value(observed_values_1, expected_values_1, "first")
+    axs[0].bar(observed_values_1.index, observed_values_1.values / len(df[column]), label='Observed')
+    axs[0].plot(expected_values_1.index, expected_values_1.values / len(df[column]), 'ro-', label='Expected')
+    axs[0].set_xlabel('First digit')
+    axs[0].set_ylabel('Frequency')
+    axs[0].legend()
+    axs[0].set_title('First position')
+
+    # Second position
+    observed_values_2 = df[column].astype(str).str[:2].value_counts().sort_values()
+    expected_values_2 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 100)], index=[str(i).zfill(2) for i in range(0, 100)]) * len(df[column])
+    p_value(observed_values_2, expected_values_2, "second")
+    axs[1].bar(observed_values_2.index, observed_values_2.values / len(df[column]), label='Observed')
+    axs[1].plot(expected_values_2.index, expected_values_2.values / len(df[column]), 'ro-', label='Expected')
+    axs[1].set_xlabel('Second digit')
+    axs[1].set_ylabel('Frequency')
+    axs[1].legend()
+    axs[1].set_title('Second position')
+
+    # Third position
+    observed_values_3 = df[column].astype(str).str[:3].value_counts().sort_values()
+    expected_values_3 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 1000)], index=[str(i).zfill(3) for i in range(0, 1000)]) * len(df[column])
+    p_value(observed_values_3, expected_values_3, "third")
+    axs[2].bar(observed_values_3.index, observed_values_3.values / len(df[column]), label='Observed')
+    axs[2].plot(expected_values_3.index, expected_values_3.values / len(df[column]), 'ro-', label='Expected')
+    axs[2].set_xlabel('Third digit')
+    axs[2].set_ylabel('Frequency')
+    axs[2].legend()
+    axs[2].set_title('Third position')
+    
+    plt.tight_layout()
+    st.pyplot(fig)
+
 
 st.title("## Benford's Law Test")
 
