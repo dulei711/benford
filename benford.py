beb6b1c78281b1c474fc126131e72767203e10ad
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-10 16:44:46 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -1,23 +1,27 @@
-import streamlit as st
 import pandas as pd
 import numpy as np
-from scipy.stats import chi2_contingency
-from scipy.stats import kstest
 import matplotlib.pyplot as plt
-
-def p_value(observed_values, expected_values, digit):
-    test_statistic, p_value = kstest(observed_values, expected_values)
-    if p_value < 0.05:
-        st.write("The column", column, "may contain fraudulent data (p-value =", p_value, ") on the ", digit, "digit")
-    else:
-        st.write("The column", column, "does not seem to contain fraudulent data (p-value =", p_value, ") on the ", digit, "digit")
 
 def benfords_law_test(df, column):    
     number_counts = df[column].value_counts()
+    st.dataframe(muber_counts)
     count_df = pd.DataFrame({'Number': number_counts.index, 'Count': number_counts.values})
     count_df['Observed Frequency'] = count_df['Count'] / count_df['Count'].sum()
     count_df['Expected Frequency'] = np.log10(1 + 1/count_df['Number']) / np.log10(10)
-    return st.dataframe(count_df)
+
+    # Plot the observed and expected frequencies
+    plt.plot(count_df['Number'], count_df['Observed Frequency'], 'bo-', label='Observed Frequency')
+    plt.plot(count_df['Number'], count_df['Expected Frequency'], 'r^-', label='Expected Frequency')
+    plt.xscale('log')
+    plt.xlabel('Number')
+    plt.ylabel('Frequency')
+    plt.title('Benford\'s Law Test')
+    plt.legend()
+    plt.show()
+
+    # Display the dataframe
+    st.pyplot(fig)
+
     
 st.title("## Benford's Law Test")
 
