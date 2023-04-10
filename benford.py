c4e4e80821ebe4349b531a28fb9e98d2ce824dea
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-10 11:32:52 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -51,8 +51,6 @@ def benfords_law_test(df, column):
     # Third position
     observed_values_3 = df[column].astype(str).str[2].str.pad(width=3, fillchar='0').value_counts().sort_index()
     expected_values_3 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 1000)], index=[str(i) for i in range(0, 999)]) * len(df[column])
-    observed_values_3 = df[column].astype(str).str[2].str[0].value_counts().sort_index()
-    expected_values_3 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 10)], index=[str(i) for i in range(0, 10)]) * len(df[column])
     axs[2].bar(observed_values_3.index, observed_values_3.values / len(df[column]), label='Observed')
     axs[2].plot(expected_values_3.index, expected_values_3.values / len(df[column]), 'ro-', label='Expected')
     axs[2].set_xlabel('Third digit')
