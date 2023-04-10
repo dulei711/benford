285940e15f5ea8f9f2bfa061e735be5815950b82
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-10 14:49:46 -0300

    Update benford.py

--- benford.py
+++ benford.py
@@ -17,6 +17,7 @@ def benfords_law_test(df, column):
     
     # First position
     observed_values_1 = df[column].astype(str).str[0].value_counts().sort_index()
+    observed_value1 = observed_value1.astype(int).sort_values()
     expected_values_1 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 10)], index=[str(i) for i in range(1, 10)]) * len(df[column])
     p_value(observed_values_1, expected_values_1, "first")
     axs[0].bar(observed_values_1.index, observed_values_1.values / len(df[column]), label='Observed')
@@ -27,7 +28,8 @@ def benfords_law_test(df, column):
     axs[0].set_title('First position')
 
     # Second position
-    observed_values_2 = df[column].astype(str).str[:2].value_counts().sort_values()
+    observed_values_2 = df[column].astype(str).str[:2].value_counts().sort_index()
+    observed_value2 = observed_value2.astype(int).sort_values()
     expected_values_2 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 100)], index=[str(i) for i in range(0, 99)]) * len(df[column])
     p_value(observed_values_2, expected_values_2, "second")
     axs[1].bar(observed_values_2.index, observed_values_2.values / len(df[column]), label='Observed')
@@ -38,7 +40,8 @@ def benfords_law_test(df, column):
     axs[1].set_title('Second position')
 
     # Third position
-    observed_values_3 = df[column].astype(str).str[:3].value_counts().sort_values()
+    observed_values_3 = df[column].astype(str).str[:3].value_counts().sort_index()
+    observed_value3 = observed_value3.astype(int).sort_values()
     expected_values_3 = pd.Series([np.log10(1 + 1 / i) for i in range(1, 1000)], index=[str(i) for i in range(0, 999)]) * len(df[column])
     p_value(observed_values_3, expected_values_3, "third")
     axs[2].bar(observed_values_3.index, observed_values_3.values / len(df[column]), label='Observed')
